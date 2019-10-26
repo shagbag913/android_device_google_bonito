b4f2f03021e6e7fffdc43b5cba0504b705034948
Dan Pasanen <dan.pasanen@gmail.com>
2019-10-25 20:52:43 -0500

    b4s4: disable BUILD_WITHOUT_VENDOR
    Change-Id: I13d3c5c0de28ff0994abc57f441b13045adc2794

--- device-aosip.mk
+++ device-aosip.mk
@@ -22,3 +22,6 @@ endif
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# Build vendor image
+BUILD_WITHOUT_VENDOR := false
